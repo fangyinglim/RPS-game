759e0898e56d195d67ff3bbcf7f9e50be90d27c8
fangyinglim <limfy97@gmail.com>
2021-09-23 14:42:20 +0800

    added js to endgame once there are 3wins

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let computerChoice = '';
 let choiceSelected = '';
-let count = 0
-
+let count = 0;
+//STEP 1//
+//computer generates a random value from 0 to 2, which will return rock, paper or scissors//
 function computerPlay() {
     computerChoice = Math.floor(Math.random() * 3) //returns 0, 1 or 2//
     if (computerChoice === 0) {
@@ -14,44 +15,52 @@ function computerPlay() {
         computerChoice = 'scissors'
         return computerChoice
     }
-}
-//callback function 'trackScore' included to track the score after playRound is executed
+    
+};
+computerPlay();
+//STEP 2//
+//user picks their choice, after choice is selected, playRound function will be invoked//
+//callback function 'trackScore' included to track the score after playRound is executed//
+
+
+let playerClicked = document.querySelectorAll('.choice') //returns nodelist//
+// console.log(playerClicked)
+
+//registers users choice when clicked on any of the selection//
+playerClicked.forEach (item => {
+    item.addEventListener('click', choice);
+})
+//whenever user clicks, random selection by system generated//
+playerClicked.forEach (item => {
+    item.addEventListener('click', computerPlay)
+})
+
 function choice() { 
     choiceSelected = this.innerText.toLowerCase();
     playRound(choiceSelected, computerChoice, trackScore);
 }
 
-let playerClicked = document.querySelectorAll('.choice') //returns nodelist//
-// console.log(playerClicked)
-
-//whenever user clicks, random selection by system generated//
-playerClicked.forEach (item => {
-    item.addEventListener('click', computerPlay)
-})
-//registers users choice when clicked on any of the selection//
-playerClicked.forEach (item => {
-    item.addEventListener('click', choice);
-})
 
 let computerScore = 0;
 let playerScore = 0;
-
+//STEP 3//
+//playRound function is executed and scores are compared, first to 3 wins//
 function playRound(choiceSelected, computerChoice, callback) {
     console.log(choiceSelected); 
     console.log(computerChoice);
         if (choiceSelected === computerChoice) {
             console.log('tie')
         }
-        else if (choiceSelected == "rock" && computerChoice == "scissors") {
+        else if (choiceSelected === "rock" && computerChoice === "scissors") {
             console.log('win');
             playerScore++;
         }
-        else if (choiceSelected == "paper" && computerChoice == "rock") {
+        else if (choiceSelected === "paper" && computerChoice === "rock") {
             console.log('win');
             playerScore++;
 
         }
-        else if (choiceSelected == "scissors" && computerChoice == "paper") {
+        else if (choiceSelected === "scissors" && computerChoice === "paper") {
             console.log('win');
             playerScore++;
 
@@ -61,25 +70,32 @@ function playRound(choiceSelected, computerChoice, callback) {
             computerScore++;
 
         }
-    callback(); //callback function invoked after 1 game is played
 
+    callback('parameter that will pass through callback'); //callback function invoked after 1 game is played
 }
-function trackScore() {
-    if (playerScore == 3) {
+function trackScore(param1) {
+    console.log(param1); //console log param1
+    console.log(`computer: ${computerScore}`); // console log scores, will update and return scores since they are global scoped?
+    console.log(`player: ${playerScore}`);
+    if (playerScore === 3) {
         console.log('player wins')
         endGame();
     }
-    else if (computerScore == 3) {
+    else if (computerScore === 3) {
         console.log('player loses')
         endGame();
     }
-    else if (playerScore == 3 && computerScore == 3) {
+    else if (playerScore === 3 && computerScore === 3) {
         console.log('tie')
         endGame();
     }
 
 }
-
+//reset score after 3 wins//
 function endGame() {
     console.log('game ends, code to reset scores')
+    computerScore = 0;
+    playerScore = 0;
+    console.log(`computer: ${computerScore}`);
+    console.log(`player: ${playerScore}`);
 }
